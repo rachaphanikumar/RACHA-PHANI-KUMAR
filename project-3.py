00f8f65dfc37158f201c7cc9d74228462818aa64
rachaphanikumar <rachaphanikumar12@gmail.com>
2025-04-19 15:16:31 +0530

    project-3.py

--- project-3.py
+++ project-3.py
@@ -2,7 +2,7 @@ def generate_odd_series(a):
     x = a // 2 if a % 2 == 0 else (a + 1) // 2
     return [str(num) for num in range(1, 2 * x, 2)]
 
-def main():
+if __name__ == "__main__":
     try:
         a = int(input("Enter a number: "))
         if a <= 0:
@@ -11,6 +11,3 @@ def main():
         print("Odd number series:", ", ".join(odds))
     except ValueError as e:
         print("Invalid input:", e)
-
-if __name__ == "__main__":
-    main()
